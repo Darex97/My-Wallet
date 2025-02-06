3200e3f119288a872500a47832d6a4c1649420bf
Darko Stosic <stosic.darkoo@elfak.rs>
2025-02-06 18:54:01 +0100

    home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,21 @@ import useFetch from "../hooks/useFetch";
 import Navbar from "./Navbar";
 import './Home.scss'
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Wallet from "./Wallet";
+import Transactions from "./Transactions";
+import Exchange from "./Exchange";
+import { useState } from "react";
+
+
+
 
 const Home = () => {
 
     const history = useHistory();
+    const [boolWallet, setBoolWalet] = useState(true);
+    const [boolTransactions, setBoolTransactions] = useState(false);
+    const [boolExchange, setBoolExchange] = useState(false);
+
     
     let dataFromLocalStorage = JSON.parse(localStorage.getItem('id'));
     if(!dataFromLocalStorage)
@@ -16,12 +27,60 @@ const Home = () => {
 
     const { error, isPending, data: user } = useFetch('http://localhost:8000/users/'+dataFromLocalStorage)
     
+
+    const waletClick = (e) => {
+      
+      setBoolWalet(true) ;
+      setBoolTransactions(false);
+      setBoolExchange(false);  
+
+    }
+
+    const exchangeClick = (e) => {
+      
+      setBoolWalet(false) ;
+      setBoolTransactions(false);
+      setBoolExchange(true);  
+    }
+
+    const tansactionClick = (e) => {
+      
+      setBoolWalet(false) ;
+      setBoolTransactions(true);
+      setBoolExchange(false);  
+    }
+
+
     return (
       <div className="home">
         { user && <Navbar  name={user.name} />}
-        { error && <div>{ error }</div> }
+        { error && <div className="loading" >{ error }</div> }
         { isPending && <div className="loading">Loading...</div> }
-        {/* { blogs && <BlogList blogs={blogs} /> } */}
+        <div className="pick"> 
+          <div className="leftPick"> 
+
+            <div className="lBtns" onClick={waletClick}> Wallet </div>
+
+            <div className="lBtns" onClick={tansactionClick}> Transactions </div>
+
+            <div className="lBtns" onClick={exchangeClick}> Exchange rates</div>
+
+          </div>
+
+          <div className="rightPick"> 
+           
+            { user &&  boolWallet && <Wallet user={user} />  }
+
+            { user && boolTransactions && <Transactions transactions={user.transactions} />  }
+
+            { user && boolExchange && <Exchange money={user.money} />  }
+
+
+          </div>
+
+        </div>
+        
+        
       </div>
     );
   }
