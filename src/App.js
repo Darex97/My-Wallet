205dd22546947073faeb672654de021c3d549a32
Darko Stosic <stosic.darkoo@elfak.rs>
2025-02-06 14:04:46 +0100

    app comp

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+
+function App() {
+
+  
+  return (
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Switch>
+            <Route exact path ="/">
+            <Home />
+            </Route>
+            <Route path ="/login">
+              <Login/>
+            </Route>
+
+            <Route path="*">
+              <NotFound/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
