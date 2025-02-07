60cd921807a0eaa14d345414511bffa5aacd0acf
Darko Stosic <stosic.darkoo@elfak.rs>
2025-02-07 14:39:14 +0100

    delete btn

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,18 +1,37 @@
 import { formatMoney } from '../functions/moneyFormat';
 import './Transactions.scss'
 import { useEffect, useRef } from 'react';
-
+import { useHistory } from "react-router-dom";
 
 const Transactions = ({transactions}) => {
 
-    const contentRef = useRef(null); // Kreiramo ref za div
+  //const contentRef = useRef(null); // Kreiramo ref za div
+  let dataFromLocalStorage = JSON.parse(localStorage.getItem('id'));
+  const history = useHistory();
+  //   useEffect(() => {
+  //   if (contentRef.current) {
+  //     contentRef.current.scrollTop = 0; 
+  //   }
+  // }, []);
 
+
+  const handleClick = (transId) => {
+    fetch("http://localhost:8000/users/"+dataFromLocalStorage)
+    .then((res) => res.json())
+    .then((user) => {
+      const updatedTransactions = user.transactions.filter(t => t.id !== transId);
   
-    useEffect(() => {
-    if (contentRef.current) {
-      contentRef.current.scrollTop = 0; 
-    }
-  }, []);
+      fetch("http://localhost:8000/users/"+dataFromLocalStorage, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...user, transactions: updatedTransactions }) 
+      })
+        .then((res) => res.json())
+        .then((updatedUser) => console.log("Transaction deleted:", updatedUser));
+        
+    });
+    window.location.reload();
+  }
 
     
     
@@ -21,7 +40,7 @@ const Transactions = ({transactions}) => {
             
             <div className="titleTransaction"> Transactions </div>
 
-            <div className="transactionsList" ref={contentRef}>
+            <div className="transactionsList" >
 
             {transactions && transactions.map(transaction => (
                 <div className="oneTransaction" 
@@ -32,6 +51,7 @@ const Transactions = ({transactions}) => {
                     <p> Amount: {formatMoney(transaction.amount,'EUR')}</p>
                     <p> Type: { transaction.type }</p>
                     <p> Description: { transaction.description }</p>
+                    { <button className='btnDelete' onClick={() =>handleClick(transaction.id)}> Delete</button> }
                 
                 </div>
             ))}
